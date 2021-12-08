eacab6dc99c81e992da199f1c25ca50b3a6d64b8
Jerry <zhaojerry123@gmail.com>
2021-12-08 18:18:46 -0500

    transferred api to __init__

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -20,11 +20,12 @@ import os
 from .client import PicClient
 
 SECRET_KEY = b'\x020;yr\x91\x11\xbe"\x9d\xc1\x14\x91\xadf\xec'
+SERP_API_KEY= '78191b018b0d92c5359b5f3e94ce3ee722ad51e395bb32b8dafa0b55ebf92066'
 
 db = MongoEngine()
 login_manager = LoginManager()
 bcrypt = Bcrypt()
-pic_client = PicClient(os.environ.get("SERP_API_KEY"))
+pic_client = PicClient(SERP_API_KEY)
 
 from .movies.routes import movies
 from .users.routes import users
