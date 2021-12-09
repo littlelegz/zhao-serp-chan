9c22d952e55a5c3e4dc7be94d784a527b93541ff
Jerry <zhaojerry123@gmail.com>
2021-12-09 00:56:19 -0500

    changed mongodbhost

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -21,7 +21,7 @@ from .client import PicClient
 
 SECRET_KEY = b'\x020;yr\x91\x11\xbe"\x9d\xc1\x14\x91\xadf\xec'
 SERP_API_KEY= '78191b018b0d92c5359b5f3e94ce3ee722ad51e395bb32b8dafa0b55ebf92066'
-MONGODB_HOST = "mongodb+srv://admin_user:ipg5Gnc1rcdiQUVO@cluster0.66sc1.mongodb.net/388JFinal?retryWrites=true&w=majority"
+MONGODB_HOST = "mongodb+srv://admin_user:ipg5Gnc1rcdiQUVO@cluster0.66sc1.mongodb.net/Cluster0?retryWrites=true&w=majority"
 
 db = MongoEngine()
 login_manager = LoginManager()
